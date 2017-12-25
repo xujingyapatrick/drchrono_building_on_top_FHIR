a0a37578f7c03f8268c28ad968bf809d0a9c9810
Ian Chen <chenyian@usc.edu>
2017-12-25 13:43:12 -0800

    Update views.py

--- tracker_app/views.py
+++ tracker_app/views.py
@@ -5,37 +5,21 @@ from django.views.generic.edit import CreateView
 import requests
 import api_functions
 import json
-from datetime import datetime
 from .models import trackerSystem, Patient
 from form import trackerForm
 def index(request):
 	if request.user.is_authenticated():
-		request.user.social_auth.get(provider='onpatient')
+		socialObj = request.user.social_auth.get(provider='onpatient')
 		patients = api_functions.get_patient_info(socialObj)
 		observations = api_functions.get_observation(socialObj)
 		#immunizations = api_functions.get_immunization(socialObj)
 		#conditions = api_functions.get_condition(socialObj)
 		#allergies = api_functions.get_allergy(socialObj)
 		# load all tracker data 
-		trackerData = trackerSystem.objects.filter(patient = patients[0]['id']).order_by('-Date').reverse()
-		index = 0
-		trackerDict = {}
-		trackerDict['daily_sbp'] = []
-		trackerDict['daily_dbp'] = []
-		trackerDict['daily_weight'] = []
-		trackerDict['daily_hydrate'] = []
-		trackerDict['daily_sleep'] = []
-		trackerDict['date'] = []
-		for data in trackerData:
-			trackerDict['daily_sbp'].append(data.Daily_Systolic_Blood_Pressure)
-			trackerDict['daily_dbp'].append(data.Daily_Diastolic_Blood_Pressure)
-			trackerDict['daily_weight'].append(data.Daily_Weight)
-			trackerDict['daily_hydrate'].append(data.Daily_Hydrate_Volume)
-			trackerDict['daily_sleep'].append(data.Daily_Sleep_Time)
-			trackerDict['date'].append(datetime.strftime(data.Date, "%Y-%m-%d"))
-			index += 1
-
-		return render(request, "index.html", {"patient_id" : patients[0]['id'], "patients_data" : patients, "patient_tracker_data" : json.dumps(trackerDict), "observations" : observations, "index" : (index - 1)})
+		request.session['user_id'] = patients[0]['id']
+		trackerDict = api_functions.get_trackerSystem(patients[0]['id'])
+		
+		return render(request, "index.html", {"patients_data" : patients, "patient_tracker_data" : json.dumps(trackerDict), "observations" : observations})
 	else:
 		return redirect('/')
 class trackingCreate(CreateView):
